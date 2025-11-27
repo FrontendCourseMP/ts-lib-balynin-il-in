732768cebde7803dad1204308f028c6ba1afcd5f
artem <masi.masi.2006@mail.ru>
2025-11-27 12:54:27 +0000

    add class and types, some checks and end 🐈‍

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,14 @@
-export type sum = (a: number, b: number) => number
+
+export type ValidateRule =
+  | { rule: 'required'; errorMessage: string }
+  | { rule: 'minLength'; value: number; errorMessage: string }
+  | { rule: 'maxLength'; value: number; errorMessage: string }
+  | { rule: 'email'; errorMessage: string }
+  | { rule: 'custom'; validator: (value: string) => boolean; errorMessage: string };
+
+export interface FieldValidation {
+  fieldName: string;
+  rules: ValidateRule[];
+};
+
+export type ValidationSchema = FieldValidation[];
